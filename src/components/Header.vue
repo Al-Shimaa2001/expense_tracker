e42dc301025d9fae2513498fe93e2ffc51746cb8
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-07-26 21:48:24 +0300

    refactor: restructure expense tracker application with new components and styles

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h2>Expense Tracker</h2>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Transaction {
+  id: number;
+  text: string;
+  amount: number;
+}
+
+const props = defineProps({
+  transactions: {
+    type: Array as () => Transaction[],
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<style lang="css" scoped></style>
